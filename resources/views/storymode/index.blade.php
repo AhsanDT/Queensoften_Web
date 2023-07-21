43080ea833a6abec156c1482576687adad2174f2
fahad4787 <nfahad4787@gmail.com>
2023-07-21 17:29:21 +0500

    QA fixes 21/07/2k23

--- resources/views/storymode/index.blade.php
+++ resources/views/storymode/index.blade.php
@@ -7,14 +7,23 @@
             </div>
             <div class="row">
                 <div class="col-md-12">
-                    <div class="py-3 text-right">
-                        <a href="storymode/create" class="btn">Add Season</a>
-                    </div>
+                 <div class="row py-3gi">
+                     <div class="col-sm-6">
+                         <div id="table_filter" class="dataTables_filter"><label><input type="search" class=""
+                                                                                        placeholder="Search Tutorial"
+                                                                                        aria-controls="table"></label></div>
+                     </div>
+                     <div class="col-sm-6">
+                         <div class="text-right">
+                             <a href="storymode/create" class="btn">Add Season</a>
+                         </div>
+                     </div>
+                 </div>
                     <div class="colList">
                         <div class="col">
                             <div class="box">
                                 <a href="storymode/detail" class="imgBox">
-                                    <img src="images/s1.jpg" />
+                                    <img src="images/s1.jpg"/>
                                 </a>
                                 <div class="description">Season 1</div>
                             </div>
@@ -22,7 +31,7 @@
                         <div class="col">
                             <div class="box">
                                 <a href="storymode/detail" class="imgBox">
-                                    <img src="images/s2.jpg" />
+                                    <img src="images/s2.jpg"/>
                                 </a>
                                 <div class="description">Season 2</div>
                             </div>
@@ -30,7 +39,7 @@
                         <div class="col">
                             <div class="box">
                                 <a href="storymode/detail" class="imgBox">
-                                    <img src="images/s3.jpg" />
+                                    <img src="images/s3.jpg"/>
                                 </a>
                                 <div class="description">Season 3</div>
                             </div>
@@ -38,7 +47,7 @@
                         <div class="col">
                             <div class="box">
                                 <a href="storymode/detail" class="imgBox">
-                                    <img src="images/s4.jpg" />
+                                    <img src="images/s4.jpg"/>
                                 </a>
                                 <div class="description">Season 4</div>
                             </div>
@@ -46,7 +55,7 @@
                         <div class="col">
                             <div class="box">
                                 <a href="storymode/detail" class="imgBox">
-                                    <img src="images/s5.jpg" />
+                                    <img src="images/s5.jpg"/>
                                 </a>
                                 <div class="description">Season 5</div>
                             </div>
