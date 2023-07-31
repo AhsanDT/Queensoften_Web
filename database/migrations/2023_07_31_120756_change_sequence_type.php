fff0c067bb64eeb044dc2d2231b66f89686330a2
Waqar <waqaryazdani2@gmail.com>
2023-07-31 00:15:13 -0700

    change sequence data type

---
+++ database/migrations/2023_07_31_120756_change_sequence_type.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('tutorials', function (Blueprint $table) {
+            $table->integer('sequence')->nullable()->change();
+            \DB::table('tutorials')->update(['sequence' => 0]);
+            \DB::statement('ALTER TABLE tutorials MODIFY sequence INTEGER');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('tutorials', function (Blueprint $table) {
+            $table->string('sequence')->change();
+        });
+    }
+};
