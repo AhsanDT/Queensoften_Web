e0dbffbfbf76bbf047efe06227edbb0f3a9b8202
Waqar <waqaryazdani2@gmail.com>
2023-08-15 03:18:17 -0700

    item purchase

--- app/Repositories/Api/WalletApiInterface.php
+++ app/Repositories/Api/WalletApiInterface.php
@@ -8,4 +8,5 @@ interface WalletApiInterface
 {
     public function addFund($request):JsonResponse;
     public function buyCoins($request):JsonResponse;
+    public function buyEstoreItem($request):JsonResponse;
 }
--- app/Repositories/Api/WalletApiRepository.php
+++ app/Repositories/Api/WalletApiRepository.php
@@ -3,7 +3,13 @@
 namespace App\Repositories\Api;
 
 use App\Models\Coin;
+use App\Models\Deck;
+use App\Models\Joker;
+use App\Models\Shuffle;
+use App\Models\Skin;
+use App\Models\Suit;
 use App\Models\User;
+use App\Models\UserPurchase;
 use App\Models\Wallet;
 use App\Traits\ResponseTrait;
 use Illuminate\Http\JsonResponse;
@@ -63,6 +69,47 @@ class WalletApiRepository implements WalletApiInterface
                 return $this->response(false,'Insufficient funds','Something went wrong please try again later.',Response::HTTP_UNAUTHORIZED);
             }
             return $this->response(true,'Coins Purchased Successfully','',Response::HTTP_OK);
+        } catch (\Exception $e) {
+            return $this->response(false,'','Something went wrong please try again later.',Response::HTTP_UNAUTHORIZED);
+        }
+    }
+    public function buyEstoreItem($request): JsonResponse
+    {
+        try {
+            $user = User::find($request['user_id']);
+            if (!$user) {
+                return response()->json(['message' => 'User not found'], 404);
+            }
+            $type = $request['type'];
+            switch($type){
+                case "shuffle":
+                    $model = Shuffle::find($request['purchase_id']);
+                    break;
+                case "joker":
+                    $model = Joker::find($request['purchase_id']);
+                    break;
+                case "suit":
+                    $model = Suit::find($request['purchase_id']);
+                    break;
+                case "deck":
+                    $model = Deck::find($request['purchase_id']);
+                    break;
+                case "skin":
+                    $model = Skin::find($request['purchase_id']);
+                    break;
+            }
+            if ($user->coins > 0 && $user->coins >= $model->coins){
+                $userPurchase = new UserPurchase();
+                $userPurchase->user_id = $request['user_id'];
+                $userPurchase->type = $type;
+                $userPurchase->purchase_id = $model->id;
+                $userPurchase->save();
+                $user->coins -= $model->coins;
+                $user->save();
+            }else{
+                return $this->response(false,'Insufficient funds','Something went wrong please try again later.',Response::HTTP_UNAUTHORIZED);
+            }
+            return $this->response(true,'Item purchased Successfully','',Response::HTTP_OK);
         } catch (\Exception $e) {
             return $this->response(false,'','Something went wrong please try again later.',Response::HTTP_UNAUTHORIZED);
         }
