86712cc0ec25c7dfd5c93902a7d1494e4d801780
Waqar <sabaali9716@gmail.com>
2023-10-23 06:34:43 -0700

    finally completed

--- resources/views/home/tutorial.blade.php
+++ resources/views/home/tutorial.blade.php
@@ -1,41 +1,43 @@
 @extends('home.layout.app')
 @section('main')
 <main>
-    <section class="section play_queens"> <div class="sec-banner play_queen tutorial"> <h2>How to play<span
-        style="color:#531083;"> queens of 10!</span>
+    <section class="section play_queens">
+        <div class="sec-banner play_queen tutorial">
+            <h2>How to play<span style="color:#531083;"> queens of 10!</span>
         </div>
         <div class="container-fluid">
-        <div class="product_card row">
-            <div class="col-xl-3 col-md-4 col-sm-6 mb-3 ">
-            <div class="section card_contant counter ">
-            <div class="box-contant">
-                <p>Queens of Ten has 3 game modes. (Freeplay, Timed and Challenges). Each game
-                has
-                the same game flow and the only difference is the timer and unlockable
-                achievements.</p>
-                <p>Hard Mode - is an exciting way to test your quick thinking ability and luck.</p>
-                <p>Easy and Medium - lets you play the game with easier and relax way. You have the ability to skip a
-                    card.</p>
+            <div class="product_card product-card-slider row">
+                <div class="col-xl-3 col-md-4 col-sm-6 mb-3 ">
+                    <div class="section card_contant counter ">
+                        <div class="box-contant">
+                            <p>Queens of Ten has 3 game modes. (Freeplay, Timed and Challenges). Each game
+                                has
+                                the same game flow and the only difference is the timer and unlockable
+                                achievements.</p>
+                            <p>Hard Mode - is an exciting way to test your quick thinking ability and luck.</p>
+                            <p>Easy and Medium - lets you play the game with easier and relax way. You have the ability
+                                to skip a
+                                card.</p>
+                        </div>
+                        <div class="imag_box" style="height: 280px">
+                            <img src="{{asset('home/images/queencard1.png')}}" alt="img">
+                        </div>
                     </div>
-                <div class="imag_box" style="height: 280px">
-                    <img src="{{asset('home/images/queencard1.png')}}" alt="img">
-                    </div>
-                </div>
                 </div>
                 <div class="col-xl-3 col-md-4 col-sm-6 mb-3">
-                <div class="section card_contant counter">
-                    <div class="box-contant">
-                    <p>The game starts with 12 playing cards, shuffled from the deck randomly. Cards
-                    are
-                    positioned in 3 rows and 4 columns.</p>
-                    </div>
-                    <div class="imag_box">
-                        <img src="{{asset('home/images/queencard2.png')}}" alt="img" style="height: 430px; object-fit:
+                    <div class="section card_contant counter">
+                        <div class="box-contant">
+                            <p>The game starts with 12 playing cards, shuffled from the deck randomly. Cards
+                                are
+                                positioned in 3 rows and 4 columns.</p>
+                        </div>
+                        <div class="imag_box">
+                            <img src="{{asset('home/images/queencard2.png')}}" alt="img" style="height: 430px; object-fit:
                         contain">
                         </div>
                     </div>
-                    </div>
-                    <div class="col-xl-3 col-md-4 col-sm-6 mb-3">
+                </div>
+                <div class="col-xl-3 col-md-4 col-sm-6 mb-3">
                     <div class="section card_contant counter">
                         <div class="box-contant">
                             <p>The game automatically replaces the face cards, Face cards must be replaced
@@ -67,31 +69,34 @@
             </div>
         </div>
         <div class="container-fluid">
-        <div class="product_card row">
-        <div class="col-xl-3 col-md-4 col-sm-6 mb-3">
-                <div class="section card_contant counter">
-                    <div class="box-contant">
-                    <p>The move is not valid if the selected cards does not sum up to ten (10).</p>
-                    </div>
-                    <div class="imag_box">
-                        <img src="{{asset('home/images/queencard7.png')}}" alt="img" style="height: 430px; object-fit:
-                        contain">
-                        </div>
-                    </div>
-                    </div>
+            <div class="product_card product-card-slider row">
                 <div class="col-xl-3 col-md-4 col-sm-6 mb-3">
-                <div class="section card_contant counter">
-                    <div class="box-contant">
-                    <p>When a move is valid, the game will automatically draw two (2)  cards on the deck and put them on top of the card that was selected (Hard Mode). </p>
-                    <p>In the Easy and Medium difficulty. The card that is selected in the field will be replaced by the card selected on the corresponding deck (left or right). Same rules apply.</p>
-                    </div>
-                    <div class="imag_box">
-                        <img src="{{asset('home/images/queencard8.png')}}" alt="img" style="height: 330px; object-fit:
+                    <div class="section card_contant counter">
+                        <div class="box-contant">
+                            <p>The move is not valid if the selected cards does not sum up to ten (10).</p>
+                        </div>
+                        <div class="imag_box">
+                            <img src="{{asset('home/images/queencard7.png')}}" alt="img" style="height: 430px; object-fit:
                         contain">
                         </div>
                     </div>
+                </div>
+                <div class="col-xl-3 col-md-4 col-sm-6 mb-3">
+                    <div class="section card_contant counter">
+                        <div class="box-contant">
+                            <p>When a move is valid, the game will automatically draw two (2) cards on the deck and put
+                                them on top of the card that was selected (Hard Mode). </p>
+                            <p>In the Easy and Medium difficulty. The card that is selected in the field will be
+                                replaced by the card selected on the corresponding deck (left or right). Same rules
+                                apply.</p>
+                        </div>
+                        <div class="imag_box">
+                            <img src="{{asset('home/images/queencard8.png')}}" alt="img" style="height: 330px; object-fit:
+                        contain">
+                        </div>
                     </div>
-                    <div class="col-xl-3 col-md-4 col-sm-6 mb-3">
+                </div>
+                <div class="col-xl-3 col-md-4 col-sm-6 mb-3">
                     <div class="section card_contant counter">
                         <div class="box-contant">
                             <p>A single field will become unselectable once a drawn card is a face card</p>
@@ -114,10 +119,9 @@
                 </div>
             </div>
         </div>
-        
 
         <div class="container-fluid">
-            <div class="product_card row">
+            <div class="product_card product-card-slider row">
                 <div class="col-xl-3 col-md-4 col-sm-6 mb-3">
                     <div class="section card_contant counter">
                         <div class="box-contant">
