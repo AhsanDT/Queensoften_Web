92553c747d0b1381db50933066a2f3c932ebb251
Waqar <waqaryazdani2@gmail.com>
2023-11-06 23:39:53 -0800

    facebook

--- resources/views/home/signin.blade.php
+++ resources/views/home/signin.blade.php
@@ -1,5 +1,35 @@
 @extends('home.layout.app')
 @section('main')
+    <script>
+        function openPopup(){
+            (function(d, s, id){
+                    var js, fjs = d.getElementsByTagName(s)[0];
+                    if (d.getElementById(id)) {return;}
+                    js = d.createElement(s); js.id = id;
+                    js.src = "https://connect.facebook.net/en_US/sdk.js";
+                    fjs.parentNode.insertBefore(js, fjs);
+                }(document, 'script', 'facebook-jssdk')
+            );
+
+
+            window.fbAsyncInit = function() {
+                FB.init({
+                    appId            : '185132871317943',
+                    xfbml            : true,
+                    version          : 'v18.0'
+                });
+                FB.login(function(response) {
+                    if (response.authResponse) {
+                        console.log('Welcome!  Fetching your information.... ');
+                        FB.api('/me', {fields: 'name, email'}, function(response) {
+                            document.getElementById("profile").innerHTML = "Good to see you, " + response.name + ". i see your email address is " + response.email
+                        });
+                    } else {
+                        console.log('User cancelled login or did not fully authorize.'); }
+                });
+            };
+        }
+    </script>
 <div class="authlayout">
         <div class="container">
             <div class="authbox">
@@ -17,7 +47,7 @@
                         </a>
                     </div>
                     <div class="btn-group">
-                        <a href="{{url('/auth/redirect/facebook')}}" class="btn">
+                        <a href="" onclick="openPopup()" class="btn">
                             <div class="icon-box">
                                 <img src="{{asset('images/google.svg')}}" alt="">
                             </div>
