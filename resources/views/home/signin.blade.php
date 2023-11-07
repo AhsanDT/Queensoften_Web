6319ac8b884e5dfde097b1aab9210e4f869707ae
Waqar <waqaryazdani2@gmail.com>
2023-11-06 23:49:11 -0800

    facebook

--- resources/views/home/signin.blade.php
+++ resources/views/home/signin.blade.php
@@ -1,35 +1,7 @@
 @extends('home.layout.app')
 @section('main')
-    <script>
-        function openPopup(){
-            (function(d, s, id){
-                    var js, fjs = d.getElementsByTagName(s)[0];
-                    if (d.getElementById(id)) {return;}
-                    js = d.createElement(s); js.id = id;
-                    js.src = "https://connect.facebook.net/en_US/sdk.js";
-                    fjs.parentNode.insertBefore(js, fjs);
-                }(document, 'script', 'facebook-jssdk')
-            );
-
-
-            window.fbAsyncInit = function() {
-                FB.init({
-                    appId            : '185132871317943',
-                    xfbml            : true,
-                    version          : 'v18.0'
-                });
-                FB.login(function(response) {
-                    if (response.authResponse) {
-                        console.log('Welcome!  Fetching your information.... ');
-                        FB.api('/me', {fields: 'name, email'}, function(response) {
-                            document.getElementById("profile").innerHTML = "Good to see you, " + response.name + ". i see your email address is " + response.email
-                        });
-                    } else {
-                        console.log('User cancelled login or did not fully authorize.'); }
-                });
-            };
-        }
-    </script>
+    <div id="fb-root"></div>
+    <script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v18.0&appId=185132871317943" nonce="FZ7Ns3Di"></script>
 <div class="authlayout">
         <div class="container">
             <div class="authbox">
@@ -46,14 +18,7 @@
                             Sign in with Google
                         </a>
                     </div>
-                    <div class="btn-group">
-                        <a href="" onclick="openPopup()" class="btn">
-                            <div class="icon-box">
-                                <img src="{{asset('images/google.svg')}}" alt="">
-                            </div>
-                            Sign in with Facebook
-                        </a>
-                    </div>
+                        <div class="fb-login-button" data-width="" data-size="" data-button-type="" data-layout="" data-auto-logout-link="false" data-use-continue-as="false"></div>
                     </div>
                 </form>
             </div>
