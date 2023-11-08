e360731d214d120a0fd04881f7125f492f295183
Waqar <sabaali9716@gmail.com>
2023-11-08 03:34:51 -0800

    favicon

--- resources/views/layout/head.blade.php
+++ resources/views/layout/head.blade.php
@@ -1,7 +1,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <link rel="shortcut icon" href="{{asset('images/favicon.ico')}}">
+    <link rel="shortcut icon" href="{{asset('images/logo.svg')}}">
     <title>Queens of Ten</title>
     <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
     <link media="all" rel="stylesheet" type="text/css" href="{{asset('css/main.css')}}" />
