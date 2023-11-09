264be358160f028644061f6efa8ef3b94691b0c2
Waqar <waqaryazdani2@gmail.com>
2023-11-09 04:02:58 -0800

    apple

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -132,9 +132,9 @@ class AuthController extends Controller
 
 //        dd($response->body());
         $data = json_decode($response);
-        dd($data);
-        if ($data['access_token']) {
-            $accessToken = $data['access_token'];
+//        dd($data);
+        if ($data->access_token) {
+            $accessToken = $data->access_token;
             $userResponse = Http::withToken($accessToken)->get('https://api.apple.com/userinfo');
             dd($userResponse);
             if ($userResponse->successful()) {
