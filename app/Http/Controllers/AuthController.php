499a669927af66601cfb2576ac1bdcb556addbcd
Waqar <waqaryazdani2@gmail.com>
2023-11-09 05:02:37 -0800

    apple

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -132,7 +132,7 @@ class AuthController extends Controller
 
 //        dd($response->body());
         $data = json_decode($response);
-        dd($data);
+//        dd($data);
         if ($data->access_token) {
             $accessToken = $data->access_token;
             $userResponse = Http::withToken($accessToken)->post('https://appleid.apple.com/auth/userinfo');
