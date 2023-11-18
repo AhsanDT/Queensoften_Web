c166383ab9f1b6903c8400923ebbc1bb1ad77537
Waqar <sabaali9716@gmail.com>
2023-11-17 17:03:48 -1200

    new fixes

--- resources/views/home/performence.blade.php
+++ resources/views/home/performence.blade.php
@@ -1,43 +1,64 @@
 @extends('home.layout.app')
 @section('main')
-    <section>
-        <main> <section class="box-contant"> <div class="container">
-                    <div class="box_text">
-                        <div class="title">
-                            <div class="logo"> <img src="{{asset('home/images/business1.png')}}" alt="img"></div>
-                            <div class="text">
-                                <h3>PERFORMANCE PARTNER PROGRAM</h3>
-                            </div>
-                        </div>
+        <main>   
+        <section class="box-contant">
+            <div class="container">
+                <div class="box_text">
+                    <div class="title">
+                    <div class="logo"><img src="{{asset('home/images/business3.png')}}" alt="img"></div>
+                        <div class="text"><h3>PERFORMANCE PARTNER PROGRAM</h3></div>
+                    </div>
 
-                        <div class="contant">
-                            <p>"Unlock the Power of Collaborative Marketing with Queens of Ten! In today's dynamic business
-                                landscape, successful marketing goes beyond traditional approaches. It's about forming
-                                strategic alliances that amplify your reach and engage audiences in fresh, innovative ways.
-                                That's where we come in. At Queens of Ten, we're pioneers of cross-promotion, and we're here
-                                to transform your marketing strategy. Imagine having your brand featured on high-traffic
-                                platforms, reaching new and highly targeted audiences, and forging valuable partnerships
-                                that drive results. With our cross-promotion expertise, we make this vision a reality. Our
-                                collaborative approach connects your business with complementary partners, allowing you to
-                                leverage each other's strengths. For example, if you have a fantastic new product, we'll
-                                seamlessly integrate your brand within our game while our partner showcases our business on
-                                their bustling social media and business platforms. It's a win-win for both businesses. Our
-                                cross-promotion strategy isn't just about visibility; it's about engagement, conversion, and
-                                long-term growth. Together, we'll harness the power of strategic alliances and deliver a
-                                marketing impact that truly sets you apart. Join us at Queens of Ten and let's revolutionize
-                                your marketing game!"</p>
-                            <p class="pt-3">If you are interested please visitspan <span class="dark"><a
-                                        href="">queensoften.com</a></span> or click the link below to
-                                schedule a business consultation
-                            </p>
+                    <div class="contant">
+                        <p>Are you searching for an innovative and engaging way to promote your products or services to
+                            a diverse and active audience? Look no further than "Queens of Ten," our popular card game
+                            app that captivates users worldwide. We're excited to offer your business a remarkable
+                            opportunity to reach potential customers through our platform. With millions of downloads
+                            and active players, our game provides an ideal advertising space that can elevate your brand
+                            to new heights.</p>
+                        </p>
+                            <h6 style="text-transform: none;font-weight: 800;">Why Advertise with Us?</h6>
+
+                            <ul class="dots">
+                                <li>Targeted Exposure: "Queens of Ten" appeals to a broad demographic, allowing you to
+                                    precisely target your audience based on their gaming interests and behavior. </li>
+                                <li>Engagement: Gamers are highly engaged users, and by promoting your business within
+                                    the gaming experience, you can capture their attention effectively. </li>
+                                <li>Global Reach: Our game boasts a global user base, providing you with the opportunity
+                                    to expand your brand's reach beyond borders.</li>
+                            </ul>
+
+                        <p>The Benefits of Partnering with "Queens of Ten" By joining our business program, you can
+                            unlock a world of promotional opportunities:</p>
+                        <ul class="numbers">
+                            <li>In-Game Advertising: Feature your products or services prominently within our game,
+                                ensuring that players encounter your brand during their gaming sessions. </li>
+                            <li>Sponsored Events and Challenges: Collaborate with us to create exciting in game events
+                                or challenges that revolve around your offerings, increasing user engagement and brand
+                                exposure. </li>
+                            <li>Data-Driven Insights: Gain access to valuable user data and analytics to understand how
+                                your advertisements perform and optimize your marketing strategies. </li>
+                            <li>Flexible Advertising Packages: We offer a range of advertising packages tailored to your
+                                budget and goals, ensuring that you receive the best return on investment.</li>
+                        </ul>
+
+                            <p class="pt-3">Join Us Today and Elevate Your Brand<br>
+                            Embrace the power of interactive advertising and take your brand to the next level with
+                                "Queens of Ten." Our business program offers a unique opportunity to connect with gamers
+                                and potential customers in a way that's both fun and memorable. Contact us today to
+                                explore the possibilities and let us help you craft a personalized advertising campaign
+                                that will leave a lasting impression. Don't miss out on this exciting opportunity to
+                                showcase your business within our immersive gaming experience. Partner with us and watch
+                                your brand shine in the world of "Queens of Ten.<br>If you are interested please visitspan <span class="dark"><a href="">queensoften.com</a></span> or
+                                click the link below to
+                                schedule a business consultation</p>
                             <a class="btn" href="./tutorial.php">Apply Here</a>
                             <p class="pt-3">Thank you for choosing our services!</p>
                         </div>
                     </div>
                 </div>
-            </section>
+            </div>
+        </section>
         </main>
-    </section>
-
     </div>
 @endsection
